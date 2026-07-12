2530d659cd0e23e2a6b2897076035b6f4851ec50
FUFLIIK <polleru848@gmail.com>
2026-07-12 20:43:57 +0200

    загрузка из интерфейса username

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,26 @@ mod mw_event;
 mod minecraft;
 mod directories;
 mod auth;
-
+use auth::{load_username, save_username};
 slint::include_modules!();
 
 
 #[tokio::main]
 async fn main() {
-    let mw = MainWindow::new().unwrap();
-    let mw_weak = mw.as_weak();
-    mw.set_play_button_status("Играть".into());
-    mw.on_button_play_run(move || {
+    let ui = MainWindow::new().unwrap();
+
+    let username = load_username();
+    ui.set_username(username.clone().into());
+
+    ui.on_save_username(move |username: slint::SharedString| {
+        println!("Сохранение username: {}", username);
+        let _ = save_username(username);
+    });
+
+
+    let mw_weak = ui.as_weak();
+    ui.set_play_button_status("Играть".into());
+    ui.on_button_play_run(move || {
 
         let mw_weak = mw_weak.clone();
 
@@ -20,5 +30,11 @@ async fn main() {
         });
     });
 
-    mw.run().unwrap();
+
+
+
+
+
+
+    ui.run().unwrap();
 }
